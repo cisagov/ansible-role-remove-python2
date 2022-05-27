ceed961e97b877dd8de5112b550fa9767ddc4d58
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-05-27 13:08:22 -0400

    Update testing configuration
    Update the Python 2 file test to include a fail if an unsupported host
    distribution is encountered. Add a test to ensure that Python 2
    packages are not installed.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,10 +2,8 @@
 
 # Standard Python Libraries
 import os
-import re
 
 # Third-Party Libraries
-import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -13,17 +11,47 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize(
-    "f", ["/usr/bin/python", "/usr/bin/python2", "/usr/bin/python2.7"]
-)
-def test_python(host, f):
-    """Ensure that python2-specific files no longer exist, except on AmazonLinux or Debian 9."""
-    # Note that r"^9(\.|$)" will match any string starting with "9.",
-    # or the string "9".
-    if host.system_info.distribution == "amzn" or (
-        host.system_info.distribution == "debian"
-        and re.match(r"^9(\.|$)", host.system_info.release) is not None
-    ):
-        assert host.file(f).exists
+def test_python2_files(host):
+    """Ensure that Python 2-specific files no longer exist, except on Amazon Linux 2."""
+    python2_files = ["/usr/bin/python", "/usr/bin/python2", "/usr/bin/python2.7"]
+    # Python 2 is required for the old version of yum used on Amazon Linux 2 so
+    # these paths should still exist on that platform.
+    if host.system_info.distribution == "amzn":
+        for file in python2_files:
+            assert host.file(file).exists
+    elif host.system_info.distribution in ["debian", "fedora", "kali", "ubuntu"]:
+        for file in python2_files:
+            assert not host.file(file).exists
     else:
-        assert not host.file(f).exists
+        assert (
+            False
+        ), f"Linux distribution {host.system_info.distribution} is not supported."
+
+
+def test_python2_packages(host):
+    """Ensure that no Python 2-specific packages are installed, except on Amazon Linux 2."""
+    # Since we do not want any of these installed we can safely combine them all
+    # into one comprehensive list.
+    python2_packages = [
+        "python-dev",
+        "python-minimal",
+        "python-unversioned-command",
+        "python",
+        "python2-dev",
+        "python2-minimal",
+        "python2.7",
+        "python2",
+    ]
+    # This should remain installed as it is required for the old version of yum
+    # used by Amazon Linux 2.
+    amazonlinux2_packages = ["python"]
+    if host.system_info.distribution == "amzn":
+        for package in amazonlinux2_packages:
+            assert host.package(package).is_installed
+    elif host.system_info.distribution in ["debian", "fedora", "kali", "ubuntu"]:
+        for package in python2_packages:
+            assert not host.package(package).is_installed
+    else:
+        assert (
+            False
+        ), f"Linux distribution {host.system_info.distribution} is not supported."
